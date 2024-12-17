a6be70c8367b61f9a0828e58d30c991dbd3df893
Julien D. <juliendidier83550@gmail.com>
2024-12-17 16:08:52 +0100

    list of movies for specific genre

--- Hadoop/Hadoop/src/main/java/MoviesCounterFromSpecificGenre.java
+++ Hadoop/Hadoop/src/main/java/MoviesCounterFromSpecificGenre.java
@@ -10,52 +10,59 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
-public class Movie {
+public class MoviesCounterFromSpecificGenre {
 
-    public static class MovieMapper extends Mapper<Object, Text, Text, LongWritable> {
+    public static class MoviesCounterFromSpecificGenreMapper extends Mapper<Object, Text, Text, Text> {
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             //movieId,title,genres
             if (value.toString().contains("\",")) {
+                //get the film name (between quotes if , in name) and the genres
+                String filmName = value.toString().split("\"")[1];
                 String[] otherAndGenre = value.toString().split("\",");
                 String genres = otherAndGenre[otherAndGenre.length - 1];
                 String[] genresArray = genres.split("\\|");
-                for (String genre : genresArray) {context.write(new Text(genre), new LongWritable(1));}
+                for (String genre : genresArray) {context.write(new Text(filmName), new Text(genre));}
             }
             else{
                 String[] classicSplit = value.toString().split(",");
+                String filmName = classicSplit[1];
                 String genres = classicSplit[classicSplit.length - 1];
                 String[] genresArray = genres.split("\\|");
-                for (String genre : genresArray) {context.write(new Text(genre), new LongWritable(1));}
+                for (String genre : genresArray) {context.write(new Text(genre), new Text(filmName));}
             }
         }
     }
 
-    public static class MovieReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+    public static class MoviesCounterFromSpecificGenreReducer extends Reducer<Text, Text, Text, Text> {
         @Override
-        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
-            long count = 0;
-            for (LongWritable value : values) {count += value.get();}
-            context.write(key, new LongWritable(count));
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            String genre = context.getConfiguration().get("custom.genre");
+            for (Text value : values) {
+                if (key.toString().equals(genre)) {context.write(key, value);}
+            }
         }
     }
 
     public static void main(String[] args) throws Exception{
-        if (args.length != 2) {
-            System.err.println("Usage: moviemapper <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: MoviesCounterFromSpecificGenre <input path> <output path> <genre>");
             System.exit(-1);
         }
 
         System.out.println("Starting MapReduce job with input: " + args[0] + ", output: " + args[1]);
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "wordcount");
 
-        job.setJarByClass(Movie.class);
-        job.setMapperClass(MovieMapper.class);
-        job.setCombinerClass(MovieReducer.class);
+        Configuration conf = new Configuration();
+        conf.set("custom.genre", args[2]);
+
+        Job job = Job.getInstance(conf, "specific_genre");
+
+        job.setJarByClass(MoviesCounterFromSpecificGenre.class);
+        job.setMapperClass(MoviesCounterFromSpecificGenreMapper.class);
+        job.setCombinerClass(MoviesCounterFromSpecificGenreReducer.class);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
+        job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
